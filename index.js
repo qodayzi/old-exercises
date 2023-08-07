f19745af594287786b16c64eb0ea76e90959309c
amin <ah5677750@gmail.com>
2023-08-07 13:26:48 +0300

    exercise1

---
+++ index.js
@@ -0,0 +1,14 @@
+document.querySelector("#button").addEventListener("click",function(){
+
+    let bill = document.querySelector("#input").value
+    if (bill > 50 && bill <= 300 ){
+        let tip = bill * 15 / 100
+        document.querySelector("#result").innerHTML = "Result: " + tip
+    }else if (bill > 300) {
+        let tip = bill * 20/100
+        document.querySelector("#result").innerHTML = "Result: " + tip
+    }else{
+        document.querySelector("#result").innerHTML = "Result: " + tip
+    }
+
+})
